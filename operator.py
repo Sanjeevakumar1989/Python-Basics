394df5a972f1d4447112b600bc2f2961870489dc
Sanjeevakumar1989 <93148605+Sanjeevakumar1989@users.noreply.github.com>
2021-11-14 18:05:36 +0530

    Learning some more topics

---
+++ operator.py
@@ -0,0 +1,55 @@
+#<---Arithmatic operator--->
+
+a = 20
+b = 15
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+#<---Assignment operator--->
+
+a = 25
+a += 5
+print(a)
+a -= 10
+print(a)
+a *= 2
+print(a)
+a /= 5
+print(a)
+a %= 5
+print(a)
+
+#<---comparission  operator--->
+
+a = 20
+b = 40
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+#<---logic operator--->
+
+a = 20
+b = 35
+print(a >= 20 and b <= 100)
+print(a >= 10 or b <= 50)
+print (not(a >= 20 and b <= 30))
+
+#<---bitwise operator--->
+a = 12
+b = 24
+print(a & b )
+print(a | b)
+print(a ^ b)
+
+
+
+
+
